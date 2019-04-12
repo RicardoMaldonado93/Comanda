fffccf785a0c053d62e3b6c7555057cdac38d58e
Ricardo <maldonadoricardo93@gmail.com>
2019-04-12 00:08:20 -0300

    90% terminado, agregado de encuenta y puliendo detalles

--- entidades/interfaces/IPedidos.php
+++ entidades/interfaces/IPedidos.php
@@ -3,5 +3,14 @@
 interface IPedidos{
 
     public static function TomarPedido($request,$response,$args);
+    public static function Cancelar( $request, $response, $args);
+    public static function ListoParaServir( $request, $response, $args);
+    public static function Entregar( $request, $response, $args);
+    public static function AgregarAPedido( $request, $response, $args);
+    public static function MostrarPedido( $request, $response, $args);
+    public static function MostrarPedidos( $request, $response, $args);
+    public static function MostrarEstado( $request, $response, $args);
+    public static function MostrarSector( $request, $response, $args);
+
 }
 ?>
--- entidades/interfaces/imesas.php
+++ entidades/interfaces/imesas.php
@@ -3,5 +3,13 @@
 interface IMesas {
     
     public static function MostrarMayorImporte($request, $response,$args);
+    public static function MostrarMenorImporte($request, $response, $args);
+    public static function MostrarMayorFacturacion($request, $response, $args);
+    public static function MostrarMenorFacturacion($request, $response, $args);
+    public static function MostrarMasUsada($request, $response, $args);
+    public static function MostrarMenosUsada($request, $response, $args);
+    public static function MostrarMayorCalificacion($request, $response, $args);
+    public static function MostrarMenorCalificacion($request, $response, $args);
+    public static function MostrarMesas($request, $response, $args);
 }
 ?>
